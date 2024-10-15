16409f92748897fd926d34b4a2ee7631d38f0b4d
James Jia <james@dydx.exchange>
2024-10-15 18:10:36 -0400

    add kapa.ai (#276)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import type { AppProps } from 'next/app'
 import * as React from 'react'
 
@@ -9,7 +10,19 @@ import '../fonts.css'
 function App({ Component, pageProps }: AppProps) {
 
   return (
-    <Component {...pageProps} />
+    <>
+      <Head>
+        <script
+          async
+          src="https://widget.kapa.ai/kapa-widget.bundle.js"
+          data-website-id="c82800f5-e43e-4afe-86c5-dc5ba554a339"
+          data-project-name="kapa.ai"
+          data-project-color="#FF0000"
+          data-project-logo="https://dydx.exchange/icon.svg"
+        ></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
   )
 }
 
