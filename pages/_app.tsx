6a6aa6add1e09ade41fe4054923735934c4ad54c
kzdydx <163130992+kzdydx@users.noreply.github.com>
2025-03-14 10:40:47 +0800

    Add GA tracking (#303)
    * Add GA tracking
    
    * setup GA property
    
    * fix md link check
    
    ---------
    
    Co-authored-by: kman <kaizhi.zhang92@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,43 @@
 import Head from 'next/head';
 import type { AppProps } from 'next/app'
 import * as React from 'react'
+import Script from 'next/script'
+
 
 import 'nextra-theme-docs/style.css'
 import '../styles.css'
 import '../fonts.css'
+
+const GA_ID = 'G-BM5F5DVTBF'  // Your GA ID here
 
 
 function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Head>
-        <script
-          async
+      <Script
+        strategy="afterInteractive"
           src="https://widget.kapa.ai/kapa-widget.bundle.js"
           data-website-id="c82800f5-e43e-4afe-86c5-dc5ba554a339"
           data-project-name="kapa.ai"
           data-project-color="#FF0000"
           data-project-logo="https://dydx.exchange/icon.svg"
-        ></script>
-      </Head>
+          data-user-analytics-fingerprint-enabled="true"
+          />
+
+              {/* Google Analytics */}
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        />
+        <Script strategy="lazyOnload" id="ga-script">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `}
+        </Script>
       <Component {...pageProps} />
     </>
   )
