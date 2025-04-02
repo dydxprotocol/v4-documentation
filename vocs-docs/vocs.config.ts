af1b2fcfeb0459f020fe0f054d0a784a952a2a64
Denis Kolodin <deniskolodin@gmail.com>
2025-04-02 17:11:24 +0200

    Add sections to quick start

--- vocs-docs/vocs.config.ts
+++ vocs-docs/vocs.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vocs'
 
 export default defineConfig({
-  title: 'Docs',
+  title: 'dYdX Documentation',
   sidebar: [
+    {
+      text: 'Use a client library',
+      link: '/client',
+      items: [
+        {
+          text: 'Quick start with Rust',
+          link: '/client/quick-start-rs',
+        },
+        {
+          text: 'Quick start with TypeScript',
+          link: '/client/quick-start-ts',
+        },
+        {
+          text: 'Quick start with Python',
+          link: '/client/quick-start-py',
+        },
+      ],
+    },
     {
       text: 'Getting Started',
       link: '/getting-started',
