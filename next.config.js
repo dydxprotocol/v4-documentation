41ba117972fe29e338552f5c677bd644e2186bd2
jaredvu <jaredvu@gmail.com>
2025-07-09 14:05:46 -0700

    landing page redirect

--- next.config.js
+++ next.config.js
@@ -5,38 +5,89 @@ const withNextra = require('nextra')({
     remarkPlugins: [],
     rehypePlugins: [],
   },
-})
+});
 
 module.exports = withNextra({
   async redirects() {
     return [
+      // Landing page redirect
+      { source: '/', destination: 'https://docs.dydx.xyz', permanent: true },
+
       // Original top-level
       { source: '/security', destination: '/other-security', permanent: true },
-      { source: '/terms_of_use_and_privacy_policy.md', destination: '/other-terms_of_use_and_privacy_policy.md', permanent: true },
+      {
+        source: '/terms_of_use_and_privacy_policy.md',
+        destination: '/other-terms_of_use_and_privacy_policy.md',
+        permanent: true,
+      },
       { source: '/FAQ', destination: '/user-faqs', permanent: true },
 
       // Architecture
-      { source: '/architecture/architectural_overview', destination: '/concepts-architecture/architectural_overview', permanent: true },
-      { source: '/architecture/indexer', destination: '/concepts-architecture/indexer', permanent: true },
+      {
+        source: '/architecture/architectural_overview',
+        destination: '/concepts-architecture/architectural_overview',
+        permanent: true,
+      },
+      {
+        source: '/architecture/indexer',
+        destination: '/concepts-architecture/indexer',
+        permanent: true,
+      },
 
       // Deposits and Withdrawals
-      { source: '/deposits_and_withdrawals/:slug*', destination: '/api_integration-deposits_and_withdrawals/:slug*', permanent: true },
+      {
+        source: '/deposits_and_withdrawals/:slug*',
+        destination: '/api_integration-deposits_and_withdrawals/:slug*',
+        permanent: true,
+      },
 
       // Developers
-      { source: '/developers/constants', destination: '/api_integration-constants', permanent: true },
-      { source: '/developers/open_source_repositories', destination: '/api_integration-repositories.md', permanent: true },
+      {
+        source: '/developers/constants',
+        destination: '/api_integration-constants',
+        permanent: true,
+      },
+      {
+        source: '/developers/open_source_repositories',
+        destination: '/api_integration-repositories.md',
+        permanent: true,
+      },
 
       // Developers - Clients
-      { source: '/developers/clients/:slug*', destination: '/api_integration-clients/:slug*', permanent: true },
+      {
+        source: '/developers/clients/:slug*',
+        destination: '/api_integration-clients/:slug*',
+        permanent: true,
+      },
 
       // Developers - Indexer
-      { source: '/developers/indexer/:slug*', destination: '/api_integration-indexer/:slug*', permanent: true },
+      {
+        source: '/developers/indexer/:slug*',
+        destination: '/api_integration-indexer/:slug*',
+        permanent: true,
+      },
 
       // Getting started
-      { source: '/getting_started/depositing_and_user_journeys', destination: 'introduction-getting_started', permanent: true },
-      { source: '/getting_started/margin_calculations', destination: 'concepts-trading/margin', permanent: true },
-      { source: '/getting_started/onboarding_faqs', destination: 'introduction-onboarding_faqs', permanent: true },
-      { source: '/getting_started/rewards_fees_and_parameters', destination: 'concepts-trading/rewards_fees_and_parameters', permanent: true },
+      {
+        source: '/getting_started/depositing_and_user_journeys',
+        destination: 'introduction-getting_started',
+        permanent: true,
+      },
+      {
+        source: '/getting_started/margin_calculations',
+        destination: 'concepts-trading/margin',
+        permanent: true,
+      },
+      {
+        source: '/getting_started/onboarding_faqs',
+        destination: 'introduction-onboarding_faqs',
+        permanent: true,
+      },
+      {
+        source: '/getting_started/rewards_fees_and_parameters',
+        destination: 'concepts-trading/rewards_fees_and_parameters',
+        permanent: true,
+      },
 
       // Governance
       { source: '/governance/:slug*', destination: '/users-governance/:slug*', permanent: true },
@@ -45,10 +96,18 @@ module.exports = withNextra({
       { source: '/guides/:slug*', destination: '/api_integration-guides/:slug*', permanent: true },
 
       // Network
-      { source: '/network/:slug*', destination: '/infrastructure_providers-network/:slug*', permanent: true },
+      {
+        source: '/network/:slug*',
+        destination: '/infrastructure_providers-network/:slug*',
+        permanent: true,
+      },
 
       // Operators
-      { source: '/operators/:slug*', destination: '/infrastructure_providers-operators/:slug*', permanent: true },
+      {
+        source: '/operators/:slug*',
+        destination: '/infrastructure_providers-operators/:slug*',
+        permanent: true,
+      },
 
       // Rewards
       { source: '/rewards/:slug*', destination: '/users-rewards/:slug*', permanent: true },
@@ -57,8 +116,16 @@ module.exports = withNextra({
       { source: '/trading:slug*', destination: '/api_integration-trading/:slug*', permanent: true },
 
       // Validators
-      { source: '/validators/:slug*', destination: '/infrastructure_providers-validators/:slug*', permanent: true },
-      { source: '/validators/upgrades/:slug*', destination: '/infrastructure_providers-validators/upgrades/:slug*', permanent: true },
-    ]
+      {
+        source: '/validators/:slug*',
+        destination: '/infrastructure_providers-validators/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/validators/upgrades/:slug*',
+        destination: '/infrastructure_providers-validators/upgrades/:slug*',
+        permanent: true,
+      },
+    ];
   },
-})
+});
