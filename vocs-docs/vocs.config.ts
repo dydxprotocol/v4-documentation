45ad97e7e0be83e7d1b738ce8171f54b1d9d00dd
gaonip <kaizhi.zhang92@gmail.com>
2025-07-17 16:54:58 +0800

    remove global seo tags (#365)

--- vocs-docs/vocs.config.ts
+++ vocs-docs/vocs.config.ts
@@ -482,7 +482,7 @@ export default defineConfig({
                         "name": "dYdX",
                         "logo": {
                             "@type": "ImageObject",
-                            "url": "https://dydx.exchange/icon.png"
+                            "url": "https://dydx.exchange/icon.svg"
                         }
                     }
                 })
@@ -546,18 +546,6 @@ export default defineConfig({
             key: 'twitter-image',
             name: 'twitter:image',
             content: 'https://dydx.exchange/icon.svg' 
-            }),
-
-        // Additional SEO meta tags
-        React.createElement('meta', {
-            key: 'description',
-            name: 'description',
-            content: 'Official dYdX documentation for developers and API traders. Learn about API trading, APIs and clients on dYdX'
-        }),
-        React.createElement('meta', {
-            key: 'keywords',
-            name: 'keywords',
-            content: 'dYdX, documentation, trading, API trading, API, DEX, perps, perpetuals'
             }),
         )
     )
