730ac1f393039715e1c18b6f22f584b6e7de7810
gaonip <kaizhi.zhang92@gmail.com>
2025-08-26 17:37:26 +0800

    Fix config bracket missing

--- vocs-docs/vocs.config.ts
+++ vocs-docs/vocs.config.ts
@@ -323,6 +323,8 @@ export default defineConfig({
                                         }
                                     ],
                                 },
+                            ],
+                        },
                     ],
                 },
                 {
